9b77b2c4b4a30db2adf9030600b37f9260a0a26f
schleising <stephen@schleising.net>
2023-07-30 16:45:47 +0100

    Use logging instead of print and register signal hanglers for SIGINT and SIGTERM

--- notify-sunset.py
+++ notify-sunset.py
@@ -1,4 +1,7 @@
+import signal
+import sys
 from time import sleep
+import logging
 
 from suntimes import suntimes
 from notify_run import Notify
@@ -6,6 +9,12 @@ import schedule
 
 class NotifySunset:
     def __init__(self) -> None:
+        # Set up logging
+        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+        # Register signal handlers
+        self._register_signal_handlers()
+
         # Set some safe defaults
         self.timeString = "09:00"
         self.timezone = "Europe/London"
@@ -16,6 +25,21 @@ class NotifySunset:
 
         # Run forever
         self._Run()
+
+    def _signal_handler(self, sig: int, _):
+        # Handle SIGINT and SIGTERM signals to ensure the Docker container stops gracefully
+        match sig:
+            case signal.SIGINT:
+                logging.info("Stopping due to keyboard interrupt...")
+                sys.exit(0)
+            case signal.SIGTERM:
+                logging.info("Stopping due to SIGTERM...")
+                sys.exit(0)
+
+    def _register_signal_handlers(self) -> None:
+        # Register signal handlers
+        signal.signal(signal.SIGINT, self._signal_handler)
+        signal.signal(signal.SIGTERM, self._signal_handler)
 
     # Function to send the notification
     def _SendSunsetTime(self) -> None:
@@ -40,7 +64,7 @@ class NotifySunset:
             self.job = schedule.every().day.at(self.timeString, self.timezone).do(self._SendSunsetTime)
 
             # Log that the job has been updated
-            print(f'Job set to run at {self.timeString} {self.timezone}')
+            logging.info(f'Job set to run at {self.timeString} {self.timezone}')
 
     def _Run(self) -> None:
         # Run forever running any pending jobs and sleeping for a second in between
